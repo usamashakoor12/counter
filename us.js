e4d83af69adfe1b29330ea8a0d127b2152e2235e
unknown <you@example.com>
2023-07-31 18:42:05 +0500

    counter

---
+++ us.js
@@ -0,0 +1,22 @@
+
+
+var add=document.getElementById("counter")
+
+let num =0
+
+function adding(){
+    num++
+    add.innerHTML=num
+}
+
+
+ function decrese(){
+    
+  num--
+    add.innerHTML=num
+}
+function cleaning(){
+    num=0
+    add.innerHTML=num
+    
+}
